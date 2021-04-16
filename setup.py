39002eb70635b921b6420f47c779f1adcdc8fde4
Max von Knobloch <max.knobloch@ingef.de>
2021-04-16 09:41:09 +0200

    typo package name in setup file

--- setup.py
+++ setup.py
@@ -3,9 +3,7 @@ from setuptools import setup
 setup(name='hdps',
       version='0.1',
       description='Functions to do high dimensional propensity score matching',
-      url='',
       author='Vivek Ramalingam Kailasam ',
       author_email='Vivek.Kailasam@ingef.de',
-      license='',
-      packages=['hpds'],
+      packages=['hdps'],
       zip_safe=False, install_requires=['pandas', 'numpy', 'epydemiology'])
