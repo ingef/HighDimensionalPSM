0e848c7b58125b7b4f4d8651ed9d9d3d75a51d3c
vivek.kailasam <vivek.kailasam@ingef.de>
2021-04-27 16:14:39 +0200

    Changes made to address the code review comments

--- hdps/__init__.py
+++ hdps/__init__.py
@@ -2,15 +2,15 @@ from hdps.algorithm_steps import get_non_code_cols, step_identify_candidate_empi
     step_assess_recurrence, step_prioritize_select_covariates
 
 
-def HDPS_implementation(Input_df, n, k, outcome, treatment, Dimension_prefixes, m=1):
-    not_code_columns = get_non_code_cols(col_names=Input_df.columns, Dimension_prefixes=Dimension_prefixes)
+def HDPS_implementation(input_df, n, k, outcome, treatment, Dimension_prefixes, m=1):
+    not_code_columns = get_non_code_cols(col_names=input_df.columns, Dimension_prefixes=Dimension_prefixes)
 
-    selected_columns = step_identify_candidate_empirical_covariates(Input_df=Input_df,
+    selected_columns = step_identify_candidate_empirical_covariates(input_df=input_df,
                                                                     Dimension_prefixes=Dimension_prefixes, n=n, m=m)
 
-    dim_covariates = step_assess_recurrence(Input_df=Input_df, selected_columns=selected_columns)
+    dim_covariates = step_assess_recurrence(input_df=input_df, selected_columns=selected_columns)
 
-    output_df, rank_df = step_prioritize_select_covariates(dim_covariates=dim_covariates, Input_df=Input_df,
+    output_df, rank_df = step_prioritize_select_covariates(dim_covariates=dim_covariates, input_df=input_df,
                                                            treatment=treatment, outcome=outcome, k=k,
                                                            not_code_columns=not_code_columns)
 
--- hdps/algorithm_steps.py
+++ hdps/algorithm_steps.py
@@ -1,52 +1,41 @@
 import math
 import numpy as np
 import pandas as pd
-import epydemiology as epy
+import logging
 
 
 def get_non_code_cols(col_names, Dimension_prefixes):
     """function that gives the name of the columns in the input df which
     doesn't need to underdo HDPS covariates selection"""
-    dim_cols = []
-    for dim_name in Dimension_prefixes:
-        for col in col_names:
-            if col.startswith(dim_name):
-                dim_cols.append(col)
-
-    not_code_columns = []
-    for col in col_names:
-        if col not in dim_cols:
-            not_code_columns.append(col)
+    not_code_columns= [column for column in col_names if
+                        not any(column.startswith(dim_name) for dim_name in Dimension_prefixes)]
 
     return not_code_columns
 
 
-def step_identify_candidate_empirical_covariates(Input_df, Dimension_prefixes, n, m=1):
+def step_identify_candidate_empirical_covariates(input_df, Dimension_prefixes, n, m=1):
     """In each dimension, it select the top n prevalent code columns"""
-    col_names = Input_df.columns
+    col_names = input_df.columns
 
     # check for duplicates
-    if np.unique(Input_df['PID']).shape[0] == Input_df['PID'].shape[0]:
-        print('No duplicates in Study_pop_treatment_outcomes')
+    if np.unique(input_df['PID']).shape[0] == input_df['PID'].shape[0]:
+        logging.info('No duplicates in PID column')
     else:
-        print('Duplicates in Study_pop_treatment_outcomes')
-        print('Error in Data')
+        logging.error('Duplicates in PID column')
+        raise Exception('Duplicates in PID column')
 
     # calculating total study population count
-    total_SP_count = Input_df.shape[0]
-    print('Total study population count is ', total_SP_count)
+    total_SP_count = input_df.shape[0]
+    #print('Total study population count is ', total_SP_count)
 
     selected_columns = []
     for dim_name in Dimension_prefixes:
 
         # getting column names for the particular dimension
-        dim_cols = []
-        for col in col_names:
-            if col.startswith(dim_name):
-                dim_cols.append(col)
+        dim_cols = [col for col in col_names if col.startswith(dim_name)]
 
         # calculating prevalance count
-        prev_count = np.count_nonzero(Input_df[dim_cols], axis=0)
+        prev_count = np.count_nonzero(input_df[dim_cols], axis=0)
         dim_prevalance = pd.DataFrame(data={dim_name: dim_cols, 'prevalance_count': prev_count})
         # Sorting dim_prevalance w.r.t count in decending order (high count first)
         dim_prevalance = dim_prevalance.sort_values(by='prevalance_count', ascending=False,
@@ -72,8 +61,8 @@ def step_identify_candidate_empirical_covariates(Input_df, Dimension_prefixes, n
     return selected_columns
 
 
-def step_assess_recurrence(Input_df, selected_columns):
-    dim_sel_covar_counts = Input_df[selected_columns]
+def step_assess_recurrence(input_df, selected_columns):
+    dim_sel_covar_counts = input_df[selected_columns]
 
     cov_dfs = []
 
@@ -98,11 +87,11 @@ def step_assess_recurrence(Input_df, selected_columns):
     return dim_covariates
 
 
-def step_prioritize_select_covariates(dim_covariates, Input_df, treatment, outcome, k, not_code_columns):
+def step_prioritize_select_covariates(dim_covariates, input_df, treatment, outcome, k, not_code_columns):
     # list of covariates' names (columns)
     covariates_list = dim_covariates.columns
 
-    dim_covariates_treat_outcome = pd.concat([Input_df[['PID', treatment, outcome]], dim_covariates], axis=1)
+    dim_covariates_treat_outcome = pd.concat([input_df[['PID', treatment, outcome]], dim_covariates], axis=1)
     # Calculation of BiasMult and abs_log_BiasMult
     Cov_BiasMult_List = []
     for covariate in covariates_list:
@@ -114,13 +103,8 @@ def step_prioritize_select_covariates(dim_covariates, Input_df, treatment, outco
         P_c1_vka = cont_tab.loc[1, 1] / cont_tab[1].sum()  # for treatment 1 (Alphabetical order)
 
         # Calculating RRcd
-        RRDB = epy.phjRelativeRisk(phjDF=dim_covariates_treat_outcome,
-                                   phjCaseVarName=outcome,
-                                   phjCaseValue=1.0,
-                                   phjRiskFactorVarName=covariate,
-                                   phjRiskFactorBaseValue=0)
-
-        RRcd = RRDB.iloc[1, 4]
+        cont_tab_co = pd.crosstab(dim_covariates_treat_outcome[covariate], dim_covariates_treat_outcome[outcome])
+        RRcd = (cont_tab_co.loc[1, 1]/cont_tab_co.loc[1, :].sum()) / (cont_tab_co.loc[0, 1]/cont_tab_co.loc[0, :].sum())
 
         # calculating absolute value of log of BiasMult
         BiasMult = (P_c1_vka * (RRcd - 1) + 1) / (P_c0_doac * (RRcd - 1) + 1)
@@ -150,9 +134,19 @@ def step_prioritize_select_covariates(dim_covariates, Input_df, treatment, outco
     dim_covariates_sel = dim_covariates_treat_outcome[sel_covariate_names]
 
     # output df
-    output_df = pd.concat([Input_df[not_code_columns], dim_covariates_sel], axis=1)
+    output_df = pd.concat([input_df[not_code_columns], dim_covariates_sel], axis=1)
 
     print('List of selected HDPS covarities (with higher to lower values of absolute log BiasMult): ',
           sel_covariate_names)
 
     return output_df, rank_df
+
+
+'''
+References
+[1] Schneeweiss, Sebastian & Rassen, Jeremy & Glynn, Robert & Avorn, Jerry & Mogun, Helen & Brookhart, M. (2009). High-Dimensional Propensity Score Adjustment in Studies of Treatment Effects Using Health Care Claims Data. Epidemiology (Cambridge, Mass.). 20. 512-22. 10.1097/EDE.0b013e3181a663cc. 
+[2] Sam Lendle, "lendle/hdps: High-dimensional propensity score algorithm". link: https://rdrr.io/github/lendle/hdps/
+[3] ohn Tazare & Ian Douglas & Elizabeth Williamson, 2019. "hdps: Implementation of high-dimensional propensity score approaches in Stata," London Stata Conference 2019 05, Stata Users Group. Link: https://www.stata.com/meeting/uk19/slides/uk19_tazare.pdf
+[4] Schneeweiss S. Automated data-adaptive analytics for electronic healthcare data to study causal treatment effects. Clin Epidemiol. 2018;10:771-788. Published 2018 Jul 6. doi:10.2147/CLEP.S166545
+[5] Wyss R, Fireman B, Rassen JA, Schneeweiss S. Erratum: High-dimensional Propensity Score Adjustment in Studies of Treatment Effects Using Health Care Claims Data. Epidemiology. 2018 Nov;29(6):e63-e64. doi: 10.1097/EDE.0000000000000886. Erratum for: Epidemiology. 2009 Jul;20(4):512-22. PMID: 29958191. 
+'''
