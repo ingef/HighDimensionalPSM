9581e21fc4de4029e17fab3fa95d9a9e46b0c487
vivek.kailasam <vivek.kailasam@ingef.de>
2021-05-31 10:36:16 +0200

    Changes: 1. Documentation added for the functions. 2. Changes of following variable names (no change in functionality). 3. Minor change of printing the threshold value in function step_prioritize_select_covariates using concatination operator '+' instead of ','.

--- hdps/__init__.py
+++ hdps/__init__.py
@@ -1,12 +1,69 @@
 from hdps.algorithm_steps import get_non_code_cols, step_identify_candidate_empirical_covariates, \
     step_assess_recurrence, step_prioritize_select_covariates
+from typing import Union
+import pandas as pd
 
 
-def HDPS_implementation(input_df, n, k, outcome, treatment, Dimension_prefixes, m=1, threshold='75p', outcome_cont=False):
-    not_code_columns = get_non_code_cols(col_names=input_df.columns, Dimension_prefixes=Dimension_prefixes)
+def HDPS_implementation(input_df: pd.DataFrame, n: int, k: int, outcome: str, treatment: str, dimension_prefixes: list, m: int =1, threshold: Union[str, float] = '75p', outcome_cont: bool =False):
+    """Performs HDPS implementation for the given data.
+
+    :param input_df: pandas.DataFrame
+        Data frame with mandatory columns - 'PID', outcome, treatment, codes (like ICD, OPS) with corresponding dimension name as prefix - examples: 'DimensionName1_ICDcodeName1', 'DimensionName1_ICDcodeName2', 'DimensionName1_ICDcodeName1', 'DimensionName2_OPScodeName1', 'DimensionName2_OPScodeName1' and
+        other optional columns of predefined and demographic columns.
+
+
+    :param n: int
+        number of prevanlent codes to be retained in each dimension. top n of prevalent codes are selected in each dimension and rest are ignored
+    :param k: int
+        number of final HDPS_covariates required. top k covariates are finally selected (considering all dimensions)
+    :param outcome: str
+        name of the column which have outcome values.
+    :param treatment: str
+        name of the column which have treatment(exposure) values. This column has to be a binary column
+    :param dimension_prefixes: list - list of strings
+        list of name of the dimensions.
+    :param m: int
+        if code occur for >= m patients, that particular code is selected else dropped in each dimension. Default value for m is 1. note: m =100 as per [1] and m =1 as per [2].
+    :param outcome_cont: bool
+        True if outcome is continous and False if outcome is binary. Default value: False
+        The HDPS implemention proposed in [1] is for binary outcome. in order to adapt the HDPS implemention for continous outcome too, below procedure is followed.
+
+        When outcome D is Binary as per [1].
+        RRcd = P(D =1 | C=1) / P(D=1|C = 0)
+
+        Adaptation when outcome D is continuous.
+        	Set threshold t which is 75th percentile value for the outcome column by default (but it could be set to median or any float or integer value if the user wishes to do so)
+        	Calculate RRcd = P(D>t|C=1) / P(D>t|C=0).
+        This is done by making a continuous outcome column to binary column, if value of outcome> t, then 1 else 0. After converting to binary, RRcd = P(D>t|C=1) / P(D>t|C=0) is equivalent to RRcd = P(D =1 | C=1) / P(D=1|C = 0).
+
+    :param threshold: Union[str, float]
+        applicable only if outcome_cont == True.
+        a cut-off threshold to make a continous outcome to binary outcome.
+        possible values for threshold are '75p', 'median', integer or float value given by the user.
+        if '75p', 75th percentile value of the outcome column is taken as cut-off threshold
+        if 'median', median value of the outcome column is taken as cut-off threshold
+        if integer or float value, the given value is taken as cut-off threshold
+
+    :return output_df: pandas.DataFrame
+        DataFrame with columns 'PID', outcome, treatment, Demographic and Predefined covariates (if given in the input_df) and columns with HDPS covariates
+
+    :return rank_df: pandas.DataFrame
+        DataFrame with columns 'Covariates Name', 'abs_log_BiasMult' and 'rank'
+        column 'Covariates Name' has names of top k HDPS covariates
+        column 'abs_log_BiasMult' has the abs(log(BiasMult)) values
+        column 'rank' has values that denotes the importance of HDPS covariates. Lower the number (rank) higher the importance. higher importance for covariates which has higher abs(log(BiasMult)) value.
+
+    References:
+    [1] Schneeweiss, Sebastian & Rassen, Jeremy & Glynn, Robert & Avorn, Jerry & Mogun, Helen & Brookhart, M. (2009). High-Dimensional Propensity Score Adjustment in Studies of Treatment Effects Using Health Care Claims Data. Epidemiology (Cambridge, Mass.). 20. 512-22. 10.1097/EDE.0b013e3181a663cc.
+    [2] Sam Lendle, "lendle/hdps: High-dimensional propensity score algorithm". link: https://rdrr.io/github/lendle/hdps/
+    [3] ohn Tazare & Ian Douglas & Elizabeth Williamson, 2019. "hdps: Implementation of high-dimensional propensity score approaches in Stata," London Stata Conference 2019 05, Stata Users Group. Link: https://www.stata.com/meeting/uk19/slides/uk19_tazare.pdf
+    [4] Schneeweiss S. Automated data-adaptive analytics for electronic healthcare data to study causal treatment effects. Clin Epidemiol. 2018;10:771-788. Published 2018 Jul 6. doi:10.2147/CLEP.S166545
+    [5] Wyss R, Fireman B, Rassen JA, Schneeweiss S. Erratum: High-dimensional Propensity Score Adjustment in Studies of Treatment Effects Using Health Care Claims Data. Epidemiology. 2018 Nov;29(6):e63-e64. doi: 10.1097/EDE.0000000000000886. Erratum for: Epidemiology. 2009 Jul;20(4):512-22. PMID: 29958191.
+    """
+    not_code_columns = get_non_code_cols(col_names=input_df.columns, dimension_prefixes=dimension_prefixes)
 
     selected_columns = step_identify_candidate_empirical_covariates(input_df=input_df,
-                                                                    Dimension_prefixes=Dimension_prefixes, n=n, m=m)
+                                                                    dimension_prefixes=dimension_prefixes, n=n, m=m)
 
     dim_covariates = step_assess_recurrence(input_df=input_df, selected_columns=selected_columns)
 
